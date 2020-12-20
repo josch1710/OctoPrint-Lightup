bba981103cb0e2cfdf0a832b262acd801b65a201
Jochen Schäfer <josch1710@live.de>
2020-12-20 11:48:19 +0100

    Implemented basic lighting

--- octoprint_LightUp/__init__.py
+++ octoprint_LightUp/__init__.py
@@ -10,10 +10,71 @@ from __future__ import absolute_import
 # Take a look at the documentation on what other plugin mixins are available.
 
 import octoprint.plugin
+from octoprint.events import Events
 
 class LightupPlugin(octoprint.plugin.SettingsPlugin,
                     octoprint.plugin.AssetPlugin,
-                    octoprint.plugin.TemplatePlugin):
+                    octoprint.plugin.TemplatePlugin,
+					octoprint.plugin.ProgressPlugin,
+					octoprint.plugin.EventHandlerPlugin):
+
+	def __init__(self):
+		super().__init__()
+		self.started = False
+		self.ledcount = 10
+		self.sequential = True
+		self.lastProgress = 0
+
+	##~~ EventHandlerPlugin mixin
+	def on_event(self, event, payload):
+		if event == Events.PRINT_STARTED:
+			self.started = True
+			self.lastProgress = 0
+			self.__sendStartStop(event)
+
+		elif event in (Events.PRINT_FAILED, Events.PRINT_CANCELLED, Events.PRINT_DONE):
+			self.started = False
+			self.lastProgress = 0
+			self.__sendStartStop(event)
+
+	def __sendStartStop(self, event):
+		if event in (Events.PRINT_DONE, Events.PRINT_STARTED):
+			self._printer.commands("M150 R0 U255 B0")
+			self._logger.info("M150 R0 U255 B0")
+		elif event == Events.PRINT_CANCELLED:
+			self._printer.commands("M150 R255 U165 B0")
+			self._logger.info("M150 R255 U165 B0")
+		else:
+			self._printer.commands("M150 R255 U0 B0")
+			self._logger.info("M150 R255 U0 B0")
+
+	##~~ ProgressPlugin mixin
+	def on_print_progress(self, storage, path, progress):
+		# If the print is not started or progress did not change since the last time, we stop here
+		if not self.started or progress <= self.lastProgress:
+			self._logger.info("Nothing to do!")
+			return
+
+		self.lastProgress = progress
+
+		# If sequential, then we use the leds except the last two as a progress bar
+		if self.sequential:
+			count = self.ledcount - 2
+			progresscount = progress * count / 100
+			self._logger.info(progress)
+			self._logger.info(progresscount)
+			for i in range(count):
+				if (i+1 < progresscount):
+					self._printer.commands("M150 R255 U255 B0 I{}".format(i))
+					self._logger.info("M150 R255 U255 B0 I{}".format(i))
+				else:
+					self._printer.commands("M150 R0 U0 B0 I{}".format(i))
+					self._logger.info("M150 R0 U0 B0 I{}".format(i))
+		# If not sequential, then we use a color gradient
+		else:
+			color = progress * 2.55
+			self._printer.commands("M150 R{} U{} B0".format(color, color))	
+			self._logger.info("M150 R{} U{} B0".format(color, color))	
 
 	##~~ SettingsPlugin mixin
 
@@ -55,6 +116,9 @@ class LightupPlugin(octoprint.plugin.SettingsPlugin,
 			)
 		)
 
+	def on_after_startup(self):
+		self._logger.info("OctoPrint-LightUp loaded!")
+
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
@@ -66,7 +130,7 @@ __plugin_name__ = "Lightup Plugin"
 # compatibility flags according to what Python versions your plugin supports!
 #__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
 #__plugin_pythoncompat__ = ">=3,<4" # only python 3
-#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 def __plugin_load__():
 	global __plugin_implementation__
