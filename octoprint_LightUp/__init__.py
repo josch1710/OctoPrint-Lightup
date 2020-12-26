a8b5de74ad1c16a14ca81f8b5ac2286b48ce8408
Jochen Schäfer <josch1710@live.de>
2020-12-26 12:41:10 +0100

    added gamma correction from Adafruit

--- octoprint_LightUp/__init__.py
+++ octoprint_LightUp/__init__.py
@@ -13,18 +13,18 @@ class LightupPlugin(octoprint.plugin.SettingsPlugin,
 	##~~ EventHandlerPlugin mixin
 	def on_event(self, event, payload):
 		if event in (Events.PRINT_DONE, Events.PRINT_STARTED):
-			self._printer.commands("M150 R0 U255 B0")
+			self.__lightLed(None, 0, 255, 0)
 			if event == Events.PRINT_DONE:
 				self.__blink['Blinking'] = False
 				self.__blink['Step'] = -1
 			#self._logger.info("M150 R0 U255 B0")
 		elif event == Events.PRINT_CANCELLED:
-			self._printer.commands("M150 R255 U165 B0")
+			self.__lightLed(None, 255, 165, 0)
 			self.__blink['Blinking'] = False
 			self.__blink['Step'] = -1
 			#self._logger.info("M150 R255 U165 B0")
 		elif event == Events.PRINT_FAILED:
-			self._printer.commands("M150 R255 U0 B0")
+			self.__lightLed(None, 255, 0, 0)
 			self.__blink['Blinking'] = False
 			self.__blink['Step'] = -1
 			#self._logger.info("M150 R255 U0 B0")
@@ -37,22 +37,20 @@ class LightupPlugin(octoprint.plugin.SettingsPlugin,
 		if self.sequential:
 			count = self.ledcount #- 2
 			progresscount = int(progress * count / 100)
+			self.__blink['Blinking'] = False # First, we set it to false, then switch it on again, if needed.
 			for i in range(count):
 				if (i == progresscount):
 					self.__blink['Blinking'] = True
 					self.__blink['Index'] = i
 					self.__doBlink()
 				elif (i < progresscount):
-					self._printer.commands("M150 R255 U255 B0 I{}".format(i))
-					#self._logger.info("M150 R255 U255 B0 I{}".format(i))
+					self.__lightLed(i, 0, 0, 255)
 				else:
-					self._printer.commands("M150 R0 U0 B0 I{}".format(i))
-					#self._logger.info("M150 R0 U0 B0 I{}".format(i))
+					self.__lightLed(i, 0, 0, 0)
 		# If not sequential, then we use a color gradient
 		else:
 			color = int(progress * 2.55)
-			self._printer.commands("M150 R{} U{} B0".format(color, color))	
-			self._logger.info("M150 R{} U{} B0".format(color, color))	
+			self.__lightLed(None, 0, 0, color)
 
 	##~~ SettingsPlugin mixin
 
@@ -137,8 +135,37 @@ class LightupPlugin(octoprint.plugin.SettingsPlugin,
 		self.sequential = self._settings.get(["sequential"])
 		self.__blink = {'Blinking': False, 'Step': -1, 'Index': -1 }
 		self.__blinkSteps = 5
+		# Table lifted from here: https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
+		self.__gamma8 = (
+			0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+			0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+			1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+			2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+			5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+			10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+			17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+			25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+			37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+			51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+			69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+			90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+			115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+			144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+			177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+			215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 )
 		self._logger.info("OctoPrint-LightUp loaded!")
 
+	def __lightLed(self, i, r, g, b):
+		try:
+			r = self.__gamma8[int(r)]
+			g = self.__gamma8[int(g)]
+			b = self.__gamma8[int(b)]
+			if i is not None:
+				self._printer.commands("M150 R{} U{} B{} I{}".format(r, g, b, i))
+			else:
+				self._printer.commands("M150 R{} U{} B{}".format(r, g, b))
+		except:
+			pass
 
 	def __doBlink(self):
 		try:
@@ -152,7 +179,7 @@ class LightupPlugin(octoprint.plugin.SettingsPlugin,
 				self.__blink['Step'] = 0
 			color = int(self.__blink['Step'] * 255 / self.__blinkSteps)
 			self.__blink['Step'] += 1
-			self._printer.commands("M150 R0 U0 B{} I{}".format(color, self.__blink['Index']))
+			self.__lightLED(self.__blink['Index'], 0, 0, color)
 			#self._logger.info("M150 R0 U0 B{} I{}".format(color, self.__blink['Index']))
 		except AttributeError:
 			pass
