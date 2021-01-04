1130aec9787f91fb2d5e8429c3f9dad43aa127b0
Jochen Schäfer <josch1710@live.de>
2021-01-04 15:51:55 +0100

    Lighting leds configuration implemented

--- octoprint_LightUp/__init__.py
+++ octoprint_LightUp/__init__.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from __future__ import absolute_import
+import re
 import octoprint.plugin
 from octoprint.events import Events
 
@@ -14,57 +15,140 @@ class LightupPlugin(octoprint.plugin.SettingsPlugin,
 	def on_event(self, event, payload):
 		if event in (Events.PRINT_DONE, Events.PRINT_STARTED):
 			self.__lightLed(None, 0, 255, 0)
-			if event == Events.PRINT_DONE:
-				self.__blink['Blinking'] = False
-				self.__blink['Step'] = -1
-			elif event == Events.PRINT_STARTED and self.sequential:
+			self.__blink['Blinking'] = False
+			self.__blink['Step'] = -1
+			if event == Events.PRINT_STARTED and self.__sequential:
 				self.__blink['Blinking'] = True
 				self.__blink['Index'] = 0
-			#self._logger.info("M150 R0 U255 B0")
 		elif event == Events.PRINT_CANCELLED:
 			self.__lightLed(None, 255, 165, 0)
 			self.__blink['Blinking'] = False
 			self.__blink['Step'] = -1
-			#self._logger.info("M150 R255 U165 B0")
 		elif event == Events.PRINT_FAILED:
 			self.__lightLed(None, 255, 0, 0)
 			self.__blink['Blinking'] = False
 			self.__blink['Step'] = -1
-			#self._logger.info("M150 R255 U0 B0")
-
-		self.__doBlink() # If we have a blinking LED, let it blink
+		else:
+			self.__doBlink() # If we have a blinking LED, let it blink
 
 	##~~ ProgressPlugin mixin
 	def on_print_progress(self, storage, path, progress):
-		# If sequential, then we use the leds ####--->not now except the last two as a progress bar
-		if self.sequential:
-			count = self.ledcount #- 2
+		# If sequential, then we use the leds as a progress bar except the ones for lighting.
+		if self.__sequential:
+			count = self.__ledcount - len(self.__ledLighting)
 			progresscount = int(progress * count / 100)
 			self.__blink['Blinking'] = False # First, we set it to false, then switch it on again, if needed.
-			for i in range(count):
-				if (i == progresscount):
-					self.__blink['Blinking'] = True
-					self.__blink['Index'] = i
-					self.__doBlink()
-				elif (i < progresscount):
-					self.__lightLed(i, 0, 0, 255)
+			index = 0 # internal counter for progress bar leds.
+			# i is global counter for all leds.
+			for i in range(self.__ledcount):
+				if i in self.__ledLighting:
+					self.__lightLed(i, 255, 255, 255)
 				else:
-					self.__lightLed(i, 0, 0, 0)
+					if index == progresscount:
+						self.__blink['Blinking'] = True
+						self.__blink['Index'] = i
+						#self._logger.info("Set Blink index {}".format(i))
+						self.__doBlink()
+					elif index < progresscount:
+						self.__lightLed(i, 0, 0, 255)
+					else:
+						self.__lightLed(i, 0, 128, 0)
+					index += 1
+
 		# If not sequential, then we use a color gradient
 		else:
 			color = int(progress * 2.55)
 			self.__lightLed(None, 0, 0, color)
 
-	##~~ SettingsPlugin mixin
+	# Helper for lighting all leds (i = None) or one Leds (i is index to that led).
+	# Brightness will be gamma corrected (cf. https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix)
+	def __lightLed(self, i, r, g, b):
+		try:
+			r = self.__gamma8[int(r)]
+			g = self.__gamma8[int(g)]
+			b = self.__gamma8[int(b)]
+			if i is None:
+				self._printer.commands("M150 R{} U{} B{}".format(r, g, b))
+				#self._logger.info("M150 R{} U{} B{}".format(r, g, b))
+			else:
+				self._printer.commands("M150 R{} U{} B{} I{}".format(r, g, b, i))
+				#self._logger.info("M150 R{} U{} B{} I{}".format(r, g, b, i))
 
+		except:
+			pass
+
+	# Helper for clearing all leds
+	def __clearLeds(self, start):
+		for i in range(start, self.__ledcount):
+			self.__lightLed(i, 0, 0, 0)
+
+	# Helper for blinking the recent progress led.
+	def __doBlink(self):
+		try:
+			if not self.__blink['Blinking']: # Blinking deactivated
+				#self._logger.info("No Blinking")
+				return
+			# We don't want to blink the lighting leds, if they are the beginning
+			while self.__blink['Index'] in self.__ledLighting:
+				self.__blink['Index'] += 1
+				#self._logger.info("Blink incr {}".format(self.__blink['Index']))
+			if self.__blinkSteps == 0:
+				self.__blinkSteps = 1
+			if self.__blink['Step'] < 0:
+				self.__blink['Step'] = 0
+			if self.__blink['Step'] >= self.__blinkSteps:
+				self.__blink['Step'] = 0
+			color = int(self.__blink['Step'] * 255 / self.__blinkSteps)
+			self.__blink['Step'] += 1
+			self.__lightLed(self.__blink['Index'], 0, 0, color)
+			#self._logger.info("M150 R0 U0 B{} I{}".format(color, self.__blink['Index']))
+		except AttributeError: #as data:
+			#self._logger.info("Exception {}".format(data))
+			pass
+
+	def __parseLighting(self, lightSetting):
+		if lightSetting is None:
+			return None
+			
+		parsed = []
+		parts = lightSetting.split(",")
+		for part in parts:
+			matches = re.findall("(\\d+)\\s*-?\\s*(\\d+)?", part.strip())
+			#self._logger.info("matches {}".format(matches))
+			# If there was no match or more than one matches, there is an error
+			if matches is None or len(matches) != 1:
+				return None
+		
+			# Settings indices are 1-based, led indices are 0-based
+			ledstart = int(matches[0][0])-1
+			ledend = None
+			if len(matches[0][1]) > 0: # there was an interval
+				ledend = int(matches[0][1]) # Here we don't decrement, because range is not open ended.
+			
+			if ledend is None:
+				parsed.append(ledstart)
+			else:
+				parsed.extend(list(range(ledstart, ledend)))
+
+		return parsed
+
+## ----- Plugin infrastructure -----
+	##~~ SettingsPlugin mixin
 	def get_settings_defaults(self):
 		return dict(
 			ledcount = 10,
-			sequential = True
+			sequential = True,
+			ledLighting = "1,3-4,10"
+		)
+
+	def get_template_vars(self):
+		return dict(
+			ledcount = self._settings.get(["ledcount"]),
+			sequential = self._settings.get(["sequential"]),
+			ledLighting = self._settings.get(["ledLighting"])
 		)
 
 	##~~ AssetPlugin mixin
-
 	def get_assets(self):
 		# Define your plugin's asset files to automatically include in the
 		# core UI here.
@@ -75,7 +159,6 @@ class LightupPlugin(octoprint.plugin.SettingsPlugin,
 		)
 
 	##~~ Softwareupdate hook
-
 	def get_update_information(self):
 		# Define the configuration for your plugin to use with the Software Update
 		# Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
@@ -96,46 +179,10 @@ class LightupPlugin(octoprint.plugin.SettingsPlugin,
 			)
 		)
 
-	##~~ Temperatur 
-	def get_temperature_reading(self, comm, parsed_temps):
-		bed = None
-		hotend = None
-		for k, v in parsed_temps.items():
-			self._logger.info(k)
-			self._logger.info(v)
-			if k == 'B':
-				bed = v
-			elif k == 'T0':
-				hotend = v
-
-		if self.sequential:
-			if bed is not None and bed[1] is not None and bed[1] > 0:
-				led_no = self.ledcount - 1
-				percentage = int(bed[0] * 2.55 / bed[1])
-				self._printer.commands("M150 I{} R{} U{} B0".format(led_no, percentage, percentage))
-				#self._logger.info("M150 I{} R{} U{} B0".format(led_no, percentage, percentage))
-
-			if hotend is not None and hotend[1] is not None and hotend[1] > 0:
-				led_no = self.ledcount - 2
-				percentage = int(hotend[0] * 2.55 / hotend[1])
-				self._printer.commands("M150 I{} R{} U{} B0".format(led_no, percentage, percentage))
-				#self._logger.info("M150 I{} R{} U{} B0".format(led_no, percentage, percentage))
-			
-		else:
-			if bed is not None and bed[1] is not None and bed[1] > 0:
-				percentage = int(bed[0] * 2.55 / bed[1])
-				self._printer.commands("M150 R{} U{} B0".format(percentage, percentage))
-				#self._logger.info("M150 R{} U{} B0".format(percentage, percentage))
-			elif hotend is not None and hotend[1] is not None and hotend[1] > 0:
-				percentage = int(hotend[0] * 2.55 / hotend[1])
-				self._printer.commands("M150 R{} U0 B{}".format(percentage, percentage))
-				#self._logger.info("M150 I{} R{} U0 B{}".format(percentage, percentage))
-
-		return parsed_temps
-
 	def on_after_startup(self):
-		self.ledcount = self._settings.get(["ledcount"])
-		self.sequential = self._settings.get(["sequential"])
+		self.__ledcount = self._settings.get(["ledcount"])
+		self.__sequential = self._settings.get(["sequential"])
+		self.__ledLighting = self.__parseLighting(self._settings.get(["ledLighting"]))
 		self.__blink = {'Blinking': False, 'Step': -1, 'Index': -1 }
 		self.__blinkSteps = 5
 		# Table lifted from here: https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
@@ -155,41 +202,10 @@ class LightupPlugin(octoprint.plugin.SettingsPlugin,
 			115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
 			144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
 			177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-			215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 )
+			215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 
+		)
 		self._logger.info("OctoPrint-LightUp loaded!")
 
-	def __lightLed(self, i, r, g, b):
-		try:
-			r = self.__gamma8[int(r)]
-			g = self.__gamma8[int(g)]
-			b = self.__gamma8[int(b)]
-			if i is not None:
-				self._printer.commands("M150 R{} U{} B{} I{}".format(r, g, b, i))
-				#self._logger.info("M150 R{} U{} B{} I{}".format(r, g, b, i))
-			else:
-				self._printer.commands("M150 R{} U{} B{}".format(r, g, b))
-				##self._logger.info("M150 R{} U{} B{}".format(r, g, b))
-		except:
-			pass
-
-	def __doBlink(self):
-		try:
-			if not self.__blink['Blinking']: # Blinking deactivated
-				self._logger.info("No Blinking")
-				return
-			if self.__blinkSteps == 0:
-				self.__blinkSteps = 1
-			if self.__blink['Step'] < 0:
-				self.__blink['Step'] = 0
-			if self.__blink['Step'] >= self.__blinkSteps:
-				self.__blink['Step'] = 0
-			color = int(self.__blink['Step'] * 255 / self.__blinkSteps)
-			self.__blink['Step'] += 1
-			self.__lightLed(self.__blink['Index'], 0, 0, color)
-			#self._logger.info("M150 R0 U0 B{} I{}".format(color, self.__blink['Index']))
-		except AttributeError: #as data:
-			#self._logger.info("Exception {}".format(data))
-			pass
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
@@ -210,6 +226,5 @@ def __plugin_load__():
 	global __plugin_hooks__
 	__plugin_hooks__ = {
 		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
-		#"octoprint.comm.protocol.temperatures.received": __plugin_implementation__.get_temperature_reading
 	}
 
